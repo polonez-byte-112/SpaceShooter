1b2cfffe66ea95ddd4ba1e2b1ee772c20a99b1cf
polonez-byte-112 <pawel.lukasz001.dev@gmail.com>
2020-11-09 15:28:00 +0100

    update

--- app/src/main/java/com/example/space_shooter/MainActivity.java
+++ app/src/main/java/com/example/space_shooter/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.space_shooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    int screenX,screenY;
+    int screenRatioX, screenRatioY;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+        Point point = new Point();
+
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        int screenX = point.x;
+        int screenY= point.y;
+
+        screenRatioX  = 1080/screenX;
+        screenRatioY  = 1920/screenY;
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+
+
+
+
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(1.0f, 0.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float height = backgroundOne.getHeight();
+                final float translationY = height * progress;
+                backgroundOne.setTranslationY(translationY);
+                backgroundTwo.setTranslationY(translationY - height);
+            }
+        });
+        animator.start();
+
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    animator.cancel();
+                    startActivity(new Intent(MainActivity.this, GameActivity.class));
+
+            }
+        });
+
     }
+
+
+
+
+
+
 }
